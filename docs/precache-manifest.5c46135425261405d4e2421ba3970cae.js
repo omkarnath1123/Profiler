f0a06cdf4f271fbde825442d2897d6f340ba757a
omkar_nath <omkarnath1123@gmail.com>
2019-03-24 12:37:34 +0530

    docs added

---
+++ docs/precache-manifest.5c46135425261405d4e2421ba3970cae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "663ec63f44b5aa5db799",
+    "url": "/Profiler/static/js/runtime~main.68bf5ade.js"
+  },
+  {
+    "revision": "b9c241f3981f8bf3a597",
+    "url": "/Profiler/static/js/main.d7fd83e4.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/Profiler/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "b9c241f3981f8bf3a597",
+    "url": "/Profiler/static/css/main.88565530.chunk.css"
+  },
+  {
+    "revision": "c191810834d3b6c2eead802475fa06a9",
+    "url": "/Profiler/index.html"
+  }
+];
